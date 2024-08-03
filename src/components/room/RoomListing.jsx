2dadc016bd468599e0958396ba2439823f213fde
patelpraveen110 <patelpraveen110@gmail.com>
2024-08-04 00:35:20 +0530

    first commit

---
+++ src/components/room/RoomListing.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Room from "./Room";
+import Footer from "../layout/Footer";
+
+const RoomListing = () => {
+  return (
+    <section className="bg-light p-2 mb-5 mt-5 shadow">
+      <Room />
+      <Footer />
+    </section>
+  );
+};
+
+export default RoomListing;
